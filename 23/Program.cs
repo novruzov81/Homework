90dc0cd9ea774a4e4585efa6c726d42b027bef49
Ali <novruzov81@gmail.com>
2023-03-06 22:38:48 +0300

    Task23

--- 23/Program.cs
+++ 23/Program.cs
@@ -4,4 +4,23 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125 */
 
+int a = TakeConsoleInt();
 
+Console.WriteLine(Cube(a));
+
+int Cube(int a)
+{
+    int cub = 0;
+    for (int i = 1; i <= a; i++)
+        cub = i * i * i;
+        Console.WriteLine(cub);
+    return cub;
+}
+
+int TakeConsoleInt(string str = "Введите число -> ")
+{
+    int num;
+    Console.Write(str);
+    int.TryParse(Console.ReadLine()!, out num);
+    return num;
+}
