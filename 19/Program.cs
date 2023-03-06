124d1071489f6f9c14819603655c6af380fd0b8e
Ali <novruzov81@gmail.com>
2023-03-06 21:20:31 +0300

    Task19

--- 19/Program.cs
+++ 19/Program.cs
@@ -10,9 +10,16 @@
 
 int a = TakeConsoleInt();
 
+if (a/10000 < 10 & a/10000 >= 1) Polindrom(a);
+else 
+Console.WriteLine ("Введите пятизначное число");
+
+void Polindrom(int a)
+{
 if (a/10000 == a%10 || a/1000%10 == a%100/10)
     Console.Write("Число является полиндромом");
 else Console.Write("Число не является полиндромом");
+}
 
 int TakeConsoleInt(string str = "Введите число -> ")
 {
