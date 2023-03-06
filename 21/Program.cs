c7257c00a1c59699a4791ebe7ee780f8c38675f9
Ali <novruzov81@gmail.com>
2023-03-06 21:52:57 +0300

    Task21

--- 21/Program.cs
+++ 21/Program.cs
@@ -5,3 +5,22 @@ A (3,6,8); B (2,1,-7), -> 15.84
 
 A (7,-5, 0); B (1,-1,9) -> 11.53 */
 
+int Ax = TakeConsoleInt();
+int Ay = TakeConsoleInt();
+int Az = TakeConsoleInt();
+int Bx = TakeConsoleInt();
+int By = TakeConsoleInt();
+int Bz = TakeConsoleInt();
+Console.WriteLine("Введите 3 координаты точки А и 3 координаты точки B");
+
+double Distance = Math.Sqrt(Math.Pow((Ax - Bx),2) + Math.Pow((Ay - By),2) + Math.Pow((Az - Bz),2));
+
+Console.WriteLine(Distance);
+
+int TakeConsoleInt(string str = "Введите координату -> ")
+{
+    int num;
+    Console.Write(str);
+    int.TryParse(Console.ReadLine()!, out num);
+    return num;
+} 
