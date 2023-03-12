062f72e64603becd70ac62948974d0cec6b8800f
Ali <novruzov81@gmail.com>
2023-03-12 13:44:16 +0300

    Task25

--- 25/Program.cs
+++ 25/Program.cs
@@ -4,7 +4,19 @@
 
 2, 4 -> 16 */
 
-int a = TakeConsoleInt();
+int a = TakeConsoleInt("Введите число ");
+int b = TakeConsoleInt("Введите степень ");
+Console.WriteLine($"Число {a} в степени {b} = {Result(a, b)}");
+
+int Result(int a, int b)
+    {
+        int result = 1;
+        for (int i = 0; i < b; i++)
+        {
+            result *= a;    
+        }
+        return result;
+    }
 
 int TakeConsoleInt(string str = "Введите число -> ")
 {
