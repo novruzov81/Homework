1c1be11c5f8e5412d3ad706b8897c8c88e13dca0
Ali <novruzov81@gmail.com>
2023-03-12 14:20:01 +0300

    Task29

---
+++ 29/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+int a = TakeConsoleInt();
+
+int[] array = CreateArray();
+for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+
+int[] CreateArray()
+{
+    int[] t_array = new int[8];
+    for (int i = 0; i < 8; i++)
+    t_array[i] = new Random().Next(0,1000);
+    return t_array;
+}
+
+int TakeConsoleInt(string str = "Введите число -> ")
+{
+    int num;
+    Console.Write(str);
+    int.TryParse(Console.ReadLine()!, out num);
+    return num;
+}
