beb4bf083fb415fb46bd573a53684ae6ad73d923
Ali <novruzov81@gmail.com>
2023-03-13 14:02:31 +0300

    Task34

---
+++ 34/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2  */
+
+int[] array = FillArray(10,100,1000);
+int even = 0;
+Print(array);
+Console.Write($"Четных чисел в массиве -> {even}");
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+        even++;
+}
+
+void Print(Array arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr.GetValue(i)} ");
+}
+
+int[] FillArray(int size, int min = 0, int max = 10)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min,max);
+    return arr;
+}
---
+++ 38/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+int[] array = FillArray;
+
+int[] FillArray(int size, int min = 0, int max = 10)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min,max);
+    return arr;
+}
