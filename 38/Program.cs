5998e2bfc5de8c65c43034268b22ed067ce7b8f5
Ali <novruzov81@gmail.com>
2023-03-13 21:41:07 +0300

    Task38

--- 38/Program.cs
+++ 38/Program.cs
@@ -2,12 +2,32 @@
 
 [3 7 22 2 78] -> 76 */
 
-int[] array = FillArray;
+double[] array = FillArray(10);
 
-int[] FillArray(int size, int min = 0, int max = 10)
+Print(array);
+
+double min = 0;
+double max = 0;
+for (int i = 0; i < array.Length; i++)
 {
-    int[] arr = new int[size];
+    if (array[i] > max)
+        max = array[i];
+    if (array[i] < min)
+        min = array[i];
+}
+Console.WriteLine();
+Console.Write($"Разница между максимальным и минимальным элементом массива = {max - min}");
+
+void Print(Array arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr.GetValue(i)} ");
+}
+
+double[] FillArray(int size, double min = 0, double max = 10)
+{
+    double[] arr = new double[size];
     for (int i = 0; i < size; i++)
-        arr[i] = new Random().Next(min,max);
+        arr[i] = new Random().NextDouble();
     return arr;
 }
