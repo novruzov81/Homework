aceb2a52e77d943427c37fc9307d592501d6ebce
Ali <novruzov81@gmail.com>
2023-03-18 19:15:00 +0300

    Task43

--- 41/Program.cs
+++ 41/Program.cs
@@ -6,7 +6,7 @@
 
 int Prompt(string message)
 {
-    System.Console.Write(message);
+    Console.Write(message);
     string value = Console.ReadLine();
     int result = Convert.ToInt32(value);
     return result;
